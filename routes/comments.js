4e78cc79371ceff5affdd43aa919f94c419750e2
jkawashiri <jkawashiri5@gmail.com>
2023-08-27 17:32:43 -0700

    implements ensureLoggedIn for various routes

--- routes/comments.js
+++ routes/comments.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
+
 const commentsCtrl = require('../controllers/comments');
 
+const ensureLoggedIn = require('../config/ensureLoggedIn');
+
 // POST /workouts/:id/comments
-router.post('/workouts/:id/comments', commentsCtrl.create);
+router.post('/workouts/:id/comments', ensureLoggedIn, commentsCtrl.create);
 // DELETE /comments:id
-router.delete('/comments/:id', commentsCtrl.delete);
+router.delete('/comments/:id', ensureLoggedIn, commentsCtrl.delete);
 
 module.exports = router;
--- routes/workouts.js
+++ routes/workouts.js
@@ -1,21 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-var workoutsCtrl = require('../controllers/workouts');
+const workoutsCtrl = require('../controllers/workouts');
+
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 // GET /workouts
 router.get('/', workoutsCtrl.index);
 // GET /workouts/new
-router.get('/new', workoutsCtrl.new);
+router.get('/new', ensureLoggedIn, workoutsCtrl.new);
 // GET /workouts/:id
 router.get('/:id', workoutsCtrl.show);
 // GET /workouts/:id/edit
-router.get('/:id/edit', workoutsCtrl.edit);
+router.get('/:id/edit', ensureLoggedIn, workoutsCtrl.edit);
 // POST /workouts
-router.post('/', workoutsCtrl.create);
+router.post('/', ensureLoggedIn, workoutsCtrl.create);
 // PUT /workouts/:id
-router.put('/:id', workoutsCtrl.update);
+router.put('/:id', ensureLoggedIn, workoutsCtrl.update);
 // DELETE /workouts/:id
-router.delete('/:id', workoutsCtrl.delete);
+router.delete('/:id', ensureLoggedIn, workoutsCtrl.delete);
 
 module.exports = router;
